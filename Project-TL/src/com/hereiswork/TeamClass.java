68881a03d932b23df66ca53d378da6073d3fbded
vinay <vinay@DESKTOP-HP8PO56>
2021-05-02 14:01:50 -0400

    Here is the project2 which i am pushing

---
+++ Project-TL/src/com/hereiswork/TeamClass.java
@@ -0,0 +1,15 @@
+package com.hereiswork;
+
+public class TeamClass {
+
+	public static void main(String[] args) {
+		
+		int i = 5;
+		
+		if(i<10) {
+			System.out.println("The Number is " + i);
+		}
+
+	}
+
+}
